338b3e79566714d04b4827b917d0986e925cd976
yamil131989 <152355713+yamil131989@users.noreply.github.com>
2024-09-10 22:37:16 -0300

    Update carts.dao.js

--- src/daos/carts.dao.js
+++ src/daos/carts.dao.js
@@ -3,7 +3,14 @@ const {cartModel} = require('../models/carts.model.js')
 const {productModel} = require('../models/carts.model.js')
 
 
-
+// export const getCarts  = async(req= request,res= response)=>{
+//     try {
+              
+//     } catch (error) {    
+//         console.log(error)
+//         return res.status(500).json({msg:'Server Error , obteniendo los carritos '})
+//     }
+// }
 
 
 
@@ -20,7 +27,7 @@ const addCart  = async(req= request,res= response)=>{
 const getCartsbyId  = async(req= request,res= response)=>{
    try {
        const {cid} = req.params
-       const cart = await cartModel.findById(cid)
+       const cart = await cartModel.findById(cid).populate('products.id')
        if(cart)
             return res.json({cart})
         res.status(404).json({msg:"Carrito no existe"})
@@ -57,6 +64,58 @@ const getCartsbyId  = async(req= request,res= response)=>{
 }
 
 
+const deleteProductinCart = async(req= request,res= response)=>{
+    try {
+        const {cid,pid} = req.params
+        const cart = await cartModel.findByIdAndUpdate(cid,{$pull:{'products':{id:pid}}},{new:true})
+        if(!cart)
+            return res.status(404).json({msg:"No se pudo eliminar el producto"})
+        return res.json({msg:"Producto eliminado",cart})
 
-module.exports = {getCartsbyId,addCart,addProductToCart}
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({msg:'Server Error , eliminando el producto al carrito '})
+    }
+}
+const updateproductinCart  = async(req= request,res= response)=>{
+    try {
+        const {cid,pid} = req.params
+        const {quantity} = req.body
+        
+        if(!quantity|| !Number.isInteger(quantity))
+            return res.status(900).json({msg:"Ingresar quantity numerico"})
+
+        const cart = await cartModel.findOneAndUpdate({_id:cid,'products.id':pid},{$set:{'products.$.quantity':quantity}},{new:true})
+        if(!cart)
+            return res.status(404).json({msg:"No se pudo eliminar el producto"})
+        return res.json({msg:"Producto modificado",cart})
+
+     } catch (error) {    
+        console.log(error)
+        return res.status(500).json({msg:'Server Error , obteniendo el carrito '})
+    }
+ }
+
+ const deleteCart  = async(req= request,res= response)=>{
+    try {
+        const {cid} = req.params
+        const cart = await cartModel.findByIdAndDelete(cid)
+        if(!cart)
+            return res.status(4049).json({msg:"No se pudo eliminar el carrito"})
+        return res.json({msg:"Carrito eliminado",cart})
+    } catch (error) {
+        
+    }
+ }
+ const getCartsbyIdHandle  = async(cid)=>{
+    try {
+        return await cartModel.findById(cid).populate('products.id')
+                             
+     } catch (error) {    
+        console.log(error)
+        
+    }
+ }
+module.exports = {getCartsbyId,addCart,addProductToCart,deleteProductinCart,updateproductinCart,deleteCart,getCartsbyIdHandle}
+
 
